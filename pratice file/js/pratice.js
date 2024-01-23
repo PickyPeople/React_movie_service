b1d23088ec57c564b32fc82bc2d3cb2d5e41e2c1
luke Jang <jjyjjh33@gmail.com>
2024-01-23 14:29:06 +0900

    it's almost

---
+++ pratice file/js/pratice.js
@@ -0,0 +1,17 @@
+// function changePic() {
+//   const picNum1 = document.getElementsByClassName("btnBlueGreen");
+//   const picNum2 = document.getElementsByClassName("btnLightBlue");
+//   const picNum3 = document.getElementsByClassName("btnOrange");
+//   const picNum4 = document.getElementsByClassName("btnPurple");
+
+//   picNum1.addEventListener("click", changePic1)
+//   picNum2.addEventListener("click", changePic2)
+//   picNum3.addEventListener("click", changePic3)
+//   picNum4.addEventListener("click", changePic4)
+// }
+
+// function changePic1(){
+//   const pic1 = document.getElementsByClassName("pic1");
+
+  
+// }
