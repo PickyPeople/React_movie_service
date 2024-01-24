807441229c9eef1662cff90a4bed548bb4014c9c
luke <jjyjjh33@gmail.com>
2024-01-25 08:41:24 +0900

    maybe last so far

---
+++ pratice file/js/pratice_buttons.js
@@ -0,0 +1,23 @@
+function newDrawPic(){
+    const pic = document.querySelector(".album img");
+    pic.src="img/국문아" + 1 + ".jpg"
+    pic.className = "pic" + 1
+  }
+
+  function newDrawPic2(){
+    const pic = document.querySelector(".album img");
+    pic.src="img/국문아2.jpg"
+    pic.className = "pic2"
+  }
+
+  function newDrawPic3(){
+    const pic = document.querySelector(".album img");
+    pic.src="img/국문아3.jpg"
+    pic.className = "pic3"
+  }
+
+  function newDrawPic4(){
+    const pic = document.querySelector(".album img");
+    pic.src="img/국문아4.jpg"
+    pic.className = "pic4"
+  }
